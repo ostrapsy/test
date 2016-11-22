bcaf96397214791343ad5044097fa43ee6838d0a
soutkari <david.siven@gmail.com>
2016-11-22 12:26:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ $(function() {
 
   		var error = 0;
   		var uage = $('#age').val();
-		var ugender = $('#gender#value]').val();
+		var ugender = $('#gender').val();
 		var ureligion = $('#religion').val();
 
   		/*if(uage == "") {
@@ -128,7 +128,7 @@ $(function() {
   			error = 1;
   			errormsg = 'Please only letters (and no spaces)';
   		}  		*/
-		if(ugender == "male") {
+		if(ugender == "") {
 			error = 1;
 			errormsg = 'Please state your gender';
 			ugender ='undefined';
