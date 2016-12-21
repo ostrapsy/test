9960f3d73355b6271f600cee91be83a3f82260c3
soutkari <david.siven@gmail.com>
2016-12-21 10:16:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -383,7 +383,26 @@ $(function() {
 		  itemSelector : '.entry',
 		  columnWidth : 10
 		});
+   /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('btn-share')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
     // Redirect, default after 180000ms = 180s = 3min
     setTimeout(function() {
