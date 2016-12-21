d7a5d4fd83f3b02abe5740f7e8df2a0faab8e7a9
soutkari <david.siven@gmail.com>
2016-12-21 10:52:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -395,20 +395,21 @@ $(function() {
 		// CLicker share-button
 	  $('.btn-share').on('click', function() {
      		document.getElementById("myDropdown").classList.toggle("show");
-  	  });
-	  window.onclick = function(event) {
-  		if (!event.target.matches('.dropbtn')) {
+  	  
+	  	window.onclick = function(event) {
+  			if (!event.target.matches('.dropbtn')) {
 
-    			var dropdowns = document.getElementsByClassName("dropdown-content");
-    			var i;
-    			for (i = 0; i < dropdowns.length; i++) {
-     				var openDropdown = dropdowns[i];
-      				if (openDropdown.classList.contains('show')) {
-        				openDropdown.classList.remove('show');
-      				}
-    			}
-  		}
-	}
+    				var dropdowns = document.getElementsByClassName("dropdown-content");
+    				var i;
+    				for (i = 0; i < dropdowns.length; i++) {
+     					var openDropdown = dropdowns[i];
+      					if (openDropdown.classList.contains('show')) {
+        					openDropdown.classList.remove('show');
+      					}
+    				}
+  			}
+		}
+	  });
     // Initalize Masonry plugin
     // For display of user and other players boxes in columns without gaps
 		$('#task').masonry({
