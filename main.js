697a9edcebdc589d9ef19a2596d23245eb8d1c3b
soutkari <david.siven@gmail.com>
2017-01-05 16:19:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -453,16 +453,16 @@ $(function() {
 	// in addition, the number of likes another person receives is adjusted, so that there is the same number of likes overall
 	switch(condition) {
 		case 1:
-			window.settings.condition_likes = settings.condition_2_likes;
-			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
+			window.settings.condition_likes = settings.condition_1_likes;
+			window.others.posts[1].likes = settings.condition_1_adjusted_likes;
 			break;
 		case 2:
 			window.settings.condition_likes = settings.condition_2_likes;
 			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
 			break;
 		case 3:
-			window.settings.condition_likes = settings.condition_2_likes;
-			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
+			window.settings.condition_likes = settings.condition_3_likes;
+			window.others.posts[1].likes = settings.condition_3_adjusted_likes;
 			break;
 	}	
 	  
