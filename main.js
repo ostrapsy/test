bfb14525192336b70111dec049db161bc7c7293a
soutkari <david.siven@gmail.com>
2017-01-06 15:44:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -35,10 +35,10 @@ $(function() {
 	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
 
     // In condition 1, user will receive 5 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_1_likes = [10000, 15000,35000,80000,150000];  
+    settings.condition_1_likes = [10000, 9999999];  
     
     // In condition 2, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
-    settings.condition_2_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
+    settings.condition_2_likes = [10000, 11000,15000,35000,80000]; 
 
 	// **Others' likes**     
 	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
