b3a9e377cdff8fede813b30ec748cf8ba8f0e6c6
soutkari <david.siven@gmail.com>
2017-01-06 15:49:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -35,17 +35,23 @@ $(function() {
 	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
 
     // In condition 1, user will receive 5 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_1_likes = [10000, 9999999];  
+    settings.condition_1_likes = [12000, 9999999]; 
+
+    // In condition 2, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
+    settings.condition_2_likes = [10000, 15000,35000,80000,1320000,150000];  
     
-    // In condition 2, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
-    settings.condition_2_likes = [10000, 11000,15000,35000,80000]; 
+    // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
+    settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
 
 	// **Others' likes**     
 	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
-	settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
-	settings.condition_2_adjusted_likes = [12000, 9999999]; //1	
+	settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
+	settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
+	settings.condition_3_adjusted_likes = [12000, 9999999]; //1	
 	
-	settings.likes_by = ['George','AncaD','Sarah','Arjen','Jane'];
+    // Usernames by which the participant will receive "likes"
+	// If group member names are changed, these should be changed accordingly.
+    settings.likes_by = ['George','AncaD','Sarah','Arjen','Jane','Nick','Dan','Heather','Ky']; 
   }
   
   
@@ -458,12 +464,14 @@ $(function() {
 		case 1:
 			window.settings.condition_likes = settings.condition_1_likes;
 			window.others.posts[1].likes = settings.condition_1_adjusted_likes;
-			var cond = 1;
 			break;
 		case 2:
 			window.settings.condition_likes = settings.condition_2_likes;
-			window.others.posts[2].likes = settings.condition_2_adjusted_likes;
-			var cond = 2;
+			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
+			break;
+		case 3:
+			window.settings.condition_likes = settings.condition_3_likes;
+			window.others.posts[1].likes = settings.condition_3_adjusted_likes;
 			break;
 	}	
 	  
