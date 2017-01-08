ae12cc92493f176a1bdd5791821f2f082f907f6f
soutkari <david.siven@gmail.com>
2017-01-08 14:33:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ $(function() {
 	
 	// **Tasklength**     
     // Length of the group introduction task in milliseconds. Can be changed to any number (in ms). Default: 180000 (3min) 
-    settings.tasklength = 180; 
+    settings.tasklength = 30000; 
 
 	
 	// **Number** **of** **"likes"**    
@@ -35,15 +35,15 @@ $(function() {
 	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
 
     // In condition 1, user will receive 5 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_1_likes = [12000, 9999999]; 
+    settings.condition_1_likes = [7000, 9999999]; 
 
     // In condition 2, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_2_likes = [10000, 15000,35000,80000,150000];  
+    settings.condition_2_likes = [5000, 10000,12000,19000,280000];  
 
 	// **Others' likes**     
 	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
-	settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
-	settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
+	settings.condition_1_adjusted_likes = [6000, 14000,15000,19000,25000]; // 9
+	settings.condition_2_adjusted_likes = [6000, 14000,15000,19000,25000]; // 5
 	
     // Usernames by which the participant will receive "likes"
 	// If group member names are changed, these should be changed accordingly.
