9f6f7f803392ee7afb237a98d30bd2361d797947
soutkari <david.siven@gmail.com>
2017-01-08 15:06:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -95,6 +95,8 @@ $(function() {
 	  $('#profiles2').show();
 	  var tpl = $('#newtmp2').html(),html = Mustache.to_html(tpl, others3);
 	  $("#profiles2").append(html);
+	  var tpl = $('#newtmp2').html(),html = Mustache.to_html(tpl, others2);
+	  $("#profiles2").append(html);
 	  $('#submit_after_profiles').on('click',function() {
 		window.location="https://www.google.com";
 	})
