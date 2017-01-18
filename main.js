3c0b3d0f3eaeca46ec4a37c4be58a80f1ce0c06d
soutkari <david.siven@gmail.com>
2017-01-18 09:45:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ $(function() {
 	  $("#profiles2").append(html);
 	  var tpl = $('#newtmp2').html(),html = Mustache.to_html(tpl, others3);
 	  $("#profiles2").append(html);
-	  var usernames = ['AncaD','Sarah','Jane','Arjen','Dan','Heather','Ky'];
+	  var usernames = ['AncaD','Sarah','Jane','Arjen'];
 	  var times = [6000, 8000,12000,19000,28000];
 	  var user = usernames;
 	  var time = times;
