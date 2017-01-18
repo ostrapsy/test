2db6f197d2c8953d950c9de30a9cdbabdb123b02
soutkari <david.siven@gmail.com>
2017-01-18 11:57:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ $(function() {
 	  for(var i=0; i<times.length; i++) 
   		{ 
   			times[i] = +times[i];
-  			themsg = names[index] + " bajsade";
+  			themsg = names[index] + " bajsade " + index;
 
   			setTimeout(function(hej) {
   				alertify.success(themsg)
